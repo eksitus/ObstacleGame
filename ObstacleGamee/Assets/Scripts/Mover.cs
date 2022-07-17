e8fe8f46f1664fd1c73607da9402b03c5f858e5a
eksitus <uvay02@gmail.com>
2022-07-17 18:23:52 +0300

    .
    16-20

--- ObstacleGamee/Assets/Scripts/Mover.cs
+++ ObstacleGamee/Assets/Scripts/Mover.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     
-    [SerializeField]float yValue = 0.001f;
-    [SerializeField] float moveSpeed = 5f;
+    
+    [SerializeField] float moveSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+     
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        float xValue = Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime;
-        float zValue = Input.GetAxis("Vertical")*moveSpeed*Time.deltaTime;
-        transform.Translate(xValue, yValue, zValue);
+    {
+        MovePlayer();
     }
+
+
+    void PrintInstruction()
+    {
+        Debug.Log("It's working");
+        Debug.Log("Really working");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(xValue, 0, zValue);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
